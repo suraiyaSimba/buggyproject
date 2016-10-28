c927d4dd601987ae9895b865f8c5b440732f24bc
nsidhaye <sidhaye.nikhil@gmail.com>
2016-10-28 09:52:32 -0700

    Inital Commit
    Init

---
+++ src/main/java/nss/buggyproject/BuggyStringUtil.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package nss.buggyproject;
+
+/**
+ * @author nikhils
+ *
+ */
+public class BuggyStringUtil {
+
+	
+	
+	public static String replaceString(String s, String sMatch, String sReplace)
+    {
+        if (sReplace == null)
+            sReplace = "";
+
+        if (sMatch == null || "".equals(sMatch) || sMatch.equals(sReplace))
+            return s;
+
+        if (s == null || s.equals(""))
+        {
+            return "";
+        }
+
+        int i = 0;
+        int j = s.indexOf(sMatch);
+
+        if (j < 0)
+        {
+            return s;
+        }
+
+        //Comemnted to introduce defect
+        //StringBuffer sb = new StringBuffer(s.length());  
+        StringBuffer sb = new StringBuffer("test"); 
+        while (true)
+        {
+            sb.append(s.substring(i, j));
+            sb.append(sReplace);
+
+            i = j + sMatch.length();
+            j = s.indexOf(sMatch, i);
+
+            if (j < 0)
+            {
+                sb.append(s.substring(i));
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+	
+	
+	
+	 public static boolean isEmpty(final CharSequence cs) {
+		 //Introduce defect
+//		         return cs == null || cs.length() == 0;
+		 return cs == null && cs.length() == 0;
+		     }
+	
+	 
+	  public static String trim(final String str) {
+		  // Introduce defect
+//		          return str == null ? null : str.trim();
+		          
+		  return str == null ? str.trim() : null;
+		      
+	  }
+		  
+}
---
+++ src/test/java/nss/buggyproject/TestDummy.java
@@ -0,0 +1,69 @@
+package nss.buggyproject;
+/**
+ * 
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+/**
+ * @author nikhils
+ *
+ */
+@RunWith(JUnitPlatform.class)
+public class TestDummy {
+
+	protected static Logger logger = Logger.getLogger("TestDummy");
+
+	@DisplayName("First Test")
+	@Test
+	public void myFirstTest() {
+		assertEquals(2, 1 + 1);
+		logger.fine("Fine Message");
+		logger.info("Info Message");
+	}
+
+	@Disabled
+	@Test
+	public void testWillBeSkipped() {
+		System.out.println("dsddsfs");
+		logger.fine("Fine Message");
+		logger.info("Info Message");
+	}
+
+	@Disabled
+	@Test
+	public void testGetStringLengthSimpleTest() {
+		assertEquals("hello:5", "hello", () -> "'hello' (length: 5) wasn't calculated properly");
+	}
+
+	@Test
+	void testInAllEnvironments() {
+		assumingThat("CI".equals(System.getenv("ENV")), () -> {
+			// perform these assertions only on the CI server
+			assertEquals(2, 3);
+			System.out.println("CI");
+		});
+
+		// perform these assertions in all environments
+		assertEquals("a string", "a string");
+		System.out.println("NOT CI");
+	}
+
+	@Test
+	void testOnlyOnDeveloperWorkstation() {
+		assumeTrue("DEV".equals(System.getenv("ENV")), "Aborting test: not on developer workstation");
+		// remainder of test
+		System.out.println("Executed...");
+	}
+
+}
